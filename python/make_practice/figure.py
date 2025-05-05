79d999fe5a3e34b6ab603767be6c5f3e84bdb64e
yanjun zhang <simpson1996@live.cn>
2025-05-05 13:55:46 +0200

    daily practice

--- python/make_practice/figure.py
+++ python/make_practice/figure.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0,10,500)
-y = np.sin(x)
+x = np.linspace(0,1,1000)
+y = np.cos(x)
 
-plt.figure(figsize = (10,8))
 plt.plot(x,y,label="sine wave")
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Generated figure')
 plt.legend()
 
-plt.savefig('figures/plot.pdf')
+plt.savefig('figures/figure.pdf')
 print('Figre generated')
